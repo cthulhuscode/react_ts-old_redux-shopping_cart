7a0ab1ea391ac84abca0503fb8faef28d7c3667d
Enrique Ruvalcaba <jordison352@gmail.com>
2023-03-30 16:22:22 -0600

    added standard and redux devtools

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,16 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import { cartReducer, productsReducer } from "./reducers";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const combinedReducers = combineReducers({
   products: productsReducer,
   cart: cartReducer,
 });
 
-export const store = createStore(combinedReducers);
+export const store = createStore(
+  combinedReducers,
+  composeWithDevTools(applyMiddleware())
+);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
