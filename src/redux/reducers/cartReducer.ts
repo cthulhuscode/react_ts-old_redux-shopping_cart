37d65aa14416fe2c53e997124f1e0803993c43b1
Enrique Ruvalcaba <jordison352@gmail.com>
2023-03-30 17:31:19 -0600

    changed cart state structure

--- src/redux/reducers/cartReducer.ts
+++ src/redux/reducers/cartReducer.ts
@@ -2,9 +2,15 @@ import { AnyAction } from "redux";
 import { Product } from "../../interfaces";
 import { ADD_PRODUCT_TO_CART } from "../types";
 
-type CartState = Product[] | null;
+interface CartState {
+  list: { product: Product; price: number; qty: number }[];
+  totalPrice: number;
+}
 
-const initialState: CartState = null;
+const initialState: CartState = {
+  list: [],
+  totalPrice: 0,
+};
 
 export const cartReducer = (state = initialState, action: AnyAction) => {
   switch (action.type) {
