b13292f01253169952021ef31c4ebd05ba7f55bd
Enrique Ruvalcaba <jordison352@gmail.com>
2023-03-31 16:52:47 -0600

    get data from redux

--- src/components/Cart/CartItem/CartItem.tsx
+++ src/components/Cart/CartItem/CartItem.tsx
@@ -1,22 +1,32 @@
+import { CartItem as ICartItem } from "../../../interfaces";
 import "./CartItem.scss";
 
-export const CartItem = () => {
+export const CartItem = ({
+  item: { product, qty, price: totalPrice },
+}: {
+  item: ICartItem;
+}) => {
+  const { title, image, price } = product;
+
   return (
     <li className="cart-i">
       <span className="cart-i__remove">X</span>
-      <img
-        className="cart-i__img"
-        src="https://m.media-amazon.com/images/I/41hh5HRWOJL._AC_SS450_.jpg"
-        alt=""
-      />
+      <img className="cart-i__img" src={image} alt={title} />
 
       <div className="cart-i__details">
         <div>
-          <h4 className="cart-i__name">Xiaomi 12</h4>
-          <span className="cart-i__price">${600}</span>
+          <h4 className="cart-i__name">{title}</h4>
+          <span className="cart-i__price">${price}</span>
         </div>
 
-        <input className="cart-i__qty" type="number" name="qty" id="qty" placeholder="1" />
+        <input
+          className="cart-i__qty"
+          type="number"
+          name="qty"
+          id="qty"
+          placeholder="1"
+          value={qty}
+        />
       </div>
     </li>
   );
